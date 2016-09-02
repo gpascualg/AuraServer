78ff5641f3b46628b2ecdc310c01f32e6169a934
gpascualg <gpascualg93@gmail.com>
2016-09-02 15:42:20 +0200

    Tests: Add more cells and udate Shinzui

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -3,14 +3,20 @@
 #include "map.hpp"
 
 #include <stdio.h>
+#include <chrono>
+
 
 class Entity
 {};
 
 int main()
 {
+    typedef std::chrono::high_resolution_clock Time;
+    typedef std::chrono::nanoseconds ns;
+
     Map<Entity*> map(0, 0, 200, 50);
 
+/*
     LOG_ALWAYS("TESTING CLUSTER MERGE");
     map.addTo2D(20, 20, new Entity());
     map.addTo2D(50, 50, new Entity());
@@ -18,28 +24,44 @@ int main()
     map.addTo2D(30, 30, new Entity());
     map.addTo2D(40, 30, new Entity());
     map.addTo2D(40, 40, new Entity());
+*/
 
-    LOG_ALWAYS("CREATING");
-    for (int x = 0; x < 100000; x += rand() / (float)RAND_MAX * 10)
+    map.addTo2D(0, 0, new Entity());
+    for (int k = 1; k <= 1000; k *= 2)
     {
-        for (int y = 0; y < 10000; y += rand() / (float)RAND_MAX * 10)
+        LOG_ALWAYS("CREATING");
+        for (float x = -50 * k; x < 50 * k; x += rand() / (float)RAND_MAX * 10)
         {
-            map.addTo2D(x, y, new Entity());
+            for (float y = -5 * k; y < 5 * k; y += rand() / (float)RAND_MAX * 10)
+            {
+                map.addTo2D(x, y, new Entity());
+            }
+        }
+
+        LOG_ALWAYS("UPDATING %d CELLS", map.size());
+        {
+            auto t0 = Time::now();
+            Cluster<Cell<Entity*>*>::get()->update(0);
+            auto t1 = Time::now();
+
+            LOG_ALWAYS("\tDONE IN %f",  std::chrono::duration_cast<ns>(t1 - t0).count() / 1000000.0f);
         }
     }
 
-    LOG_ALWAYS("UPDATING...");
-    Cluster<Cell<Entity*>*>::get()->update();
+    for (int i = 0; i < 10; ++i)
+    {
+        LOG_ALWAYS("UPDATING %d CELLS", map.size());
+        {
+            auto t0 = Time::now();
+            Cluster<Cell<Entity*>*>::get()->update(0);
+            auto t1 = Time::now();
 
-    LOG_ALWAYS("UPDATING...");
-    Cluster<Cell<Entity*>*>::get()->update();
+            LOG_ALWAYS("\tDONE IN %f",  std::chrono::duration_cast<ns>(t1 - t0).count() / 1000000.0f);
+        }
+    }
 
-    LOG_ALWAYS("UPDATING...");
-    Cluster<Cell<Entity*>*>::get()->update();
+    LOG_ALWAYS("UpdateCount == CellCount, %d == %d", updateCount, cellCount);
 
-    LOG(LOG_DEBUG, "UpdateCount == CellCount, %d == %d", updateCount, cellCount);
-
-    LOG_ALWAYS("DONE (%d cells)\n", map.size());
     getchar();
 
     return 0;
