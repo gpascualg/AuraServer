47f9f8190d81211903f3bd1d19c7c6d0a33ec9d5
gpascualg <gpascualg93@gmail.com>
2017-02-03 02:31:26 +0100

    Update

--- src/aura/server/aura_server.cpp
+++ src/aura/server/aura_server.cpp
@@ -1,6 +1,7 @@
 #include "aura_server.hpp"
 #include "aura_client.hpp"
 #include "debug.hpp"
+#include "packet.hpp"
 
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
@@ -8,9 +9,10 @@
 
 const uint16_t MAX_PACKET_LEN = 1000;
 
-void on_write(const boost::system::error_code & error, std::size_t size)
+void onSend(const boost::system::error_code & error, std::size_t size, Packet* packet)
 {
-    printf("WRITE (%d): %d\n", error.value(), size);
+    LOG(LOG_DEBUG, "Written (%d): %d", (int)error.value(), (uint16_t)size);
+    packet->destroy();
 }
 
 void AuraServer::handleAccept(AuraClient* client, const boost::system::error_code& error)
@@ -21,7 +23,17 @@ void AuraServer::handleAccept(AuraClient* client, const boost::system::error_cod
     Server::handleAccept(client, error);
     client->scheduleRead(2);
 
-    boost::asio::async_write(client->socket(), boost::asio::buffer("OK!", 3), &on_write);
+    Packet* packet = Packet::create();
+    *packet << uint16_t{ 0x7BCD };
+    *packet << uint16_t{ 0x1 };
+    *packet << uint8_t{ 0x8 };
+
+    boost::asio::async_write(client->socket(), packet->sendBuffer(), 
+        [packet](const boost::system::error_code & error, std::size_t size)
+        {
+            onSend(error, size, packet);
+        }
+    );
 }
 
 void AuraServer::handleRead(AuraClient* client, const boost::system::error_code& error, size_t size)
@@ -29,56 +41,95 @@ void AuraServer::handleRead(AuraClient* client, const boost::system::error_code&
 #if IF_LOG(LOG_PACKETS) || 1
     if (!error)
     {
-        printf("READ: %d\n", size);
-        for (int i = 0; i < client->totalRead(); ++i)
+        printf("%d - READ: %d\n", time(NULL), size);
+        for (uint16_t i = 0; i < client->packet()->size(); ++i)
         {
-            printf("%2X ", (uint8_t)client->data()[i]);
+            printf("%.2X ", (uint8_t)client->packet()->data()[i]);
         }
         printf("\n");
     }
 #endif
 
     bool disconnect = false;
+    uint16_t len = 2;
+    bool reset = false;
 
-    if (!error)
+    switch (client->readPhase())
     {
-        uint16_t len = 2;
-        bool reset = false;
+        // Opcode
+        case 1:
+            break;
 
-        switch (client->readPhase())
-        {
-            // Opcode
-            case 1:
-                break;
-
-            // Length
-            case 2:
-                len = *(uint16_t*)(client->data() + 2);
-                if (len >= MAX_PACKET_LEN)
-                {
-                    disconnect = true;
-                }
-                break;
-
-            case 3:
-                reset = true;
-                break;
-        }
-
-        if (!disconnect)
-        {
-            if (reset)
+        // Length
+        case 2:
+            len = client->packet()->peek<uint16_t>(2);
+            if (len >= MAX_PACKET_LEN)
             {
-                // TODO(gpascualg): Add packet to parsing list
-                boost::asio::async_write(client->socket(), boost::asio::buffer("OK!", 3), &on_write);
-                //boost::asio::du
+                disconnect = true;
             }
+            break;
 
-            client->scheduleRead(len, reset);
-        }
+        case 3:
+            reset = true;
+            break;
     }
-    
-    if (error || disconnect)
+
+    if (!disconnect)
+    {
+        if (reset)
+        {
+            // TODO(gpascualg): Add packet to parsing list
+            Packet* packet = Packet::create();
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+            *packet << uint16_t{ 0x0001 } << uint16_t{ 0x0 };
+            *packet << uint16_t{ 0x7BCD } << uint16_t{ 0x2 } << uint16_t{ 0x8080 };
+
+            boost::asio::async_write(client->socket(), packet->sendBuffer(), 
+                [packet](const boost::system::error_code & error, std::size_t size)
+                {
+                    onSend(error, size, packet);
+                }
+            );
+        }
+
+        client->scheduleRead(len, reset);
+    }
+    else
     {
         client->close();
     }
