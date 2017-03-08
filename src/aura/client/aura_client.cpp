fed15609a700015b832094d5c146743555c4cc56
gpascualg <gpascualg93@gmail.com>
2017-03-08 04:33:34 +0100

    Implement movement on spawn from Shinzui

--- src/aura/client/aura_client.cpp
+++ src/aura/client/aura_client.cpp
@@ -4,6 +4,7 @@
 #include "map.hpp"
 #include "map_aware_entity.hpp"
 #include "motion_master.hpp"
+#include "movement_generator.hpp"
 #include "packet.hpp"
 #include "server.hpp"
 #include "opcodes.hpp"
@@ -14,8 +15,20 @@ Packet* Entity::spawnPacket()
     Packet* packet = Packet::create((uint16_t)PacketOpcodes::ENTITY_SPAWN);
     *packet << id() << uint8_t{ 0 };
     *packet << motionMaster()->position();
-    *packet << motionMaster()->speed();
     *packet << motionMaster()->forward();
+    *packet << (uint8_t)(motionMaster()->speed() * 1000.0f);
+    
+    // Movement generator
+    if (motionMaster()->generator() && motionMaster()->generator()->hasNext())
+    {
+        *packet << (uint8_t)1;
+        *packet << motionMaster()->generator()->packet();
+    }
+    else
+    {
+        *packet << (uint8_t)0;
+    }
+
     return packet;
 }
 
